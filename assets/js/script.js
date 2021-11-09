fe1379d377b6091c5fcffa1277cf8060968a0219
saltyposaune <ewalkertbone@gmail.com>
2021-11-08 17:29:58 -0700

    five day forecast added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,11 @@ const currentTempEl = document.getElementById("temp");
 const currentWindEl = document.getElementById("wind");
 const currentHumidityEl = document.getElementById("humidity");
 const currentUVEl = document.getElementById("uv-index");
-const searchEl = document.getElementById("search-button")
+const searchEl = document.getElementById("search-button");
+let currentCity = $('#city-search').val();
+var forecastContainerEl = document.querySelector("#forecast-container");
 
 var getCurrentWeather = () => {
-    let currentCity = $('#city-search').val();
     let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=" + APIKey;
 
     axios.get(apiURL) // https://axios-http.com/docs/intro
@@ -17,7 +18,7 @@ var getCurrentWeather = () => {
         const month = currentDate.getMonth();
         const year = currentDate.getFullYear();
         cityNameEl.innerHTML = response.data.name + " " + month + '/' + day + '/' + year;
-        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " °F";
         currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
         currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
         let lat = response.data.coord.lat;
@@ -25,7 +26,6 @@ var getCurrentWeather = () => {
         let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
         axios.get(UVQueryURL)
         .then(function(response){
-            console.log(response);
             let UVIndex = document.createElement("span");
             UVIndex.setAttribute("class","uv-index")
             UVIndex.innerHTML = response.data[0].value;
@@ -45,7 +45,42 @@ var getCurrentWeather = () => {
 
 // 5-day forecast
 var fiveDayForecast = () => {
-    // display a five-day forecast
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=" + APIKey;
+    fetch(forecastURL)
+    .then(function(response) {
+        response.json()
+        .then(function(data) {
+            displayForecast(data);
+        })
+    })
+}
+
+var displayForecast = function(weather) {
+    forecastContainerEl.textContent = "";
+    var forecast = weather.list;
+    for (let i = 5; i < forecast.length; i=i+8) {
+        var dailyForecast = forecast[i];
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "card";
+        console.log(dailyForecast);
+
+        var weatherIcon = document.createElement("img");
+        weatherIcon.classList = "card-body";
+        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);
+        forecastEl.appendChild(weatherIcon);
+
+        var forecastTempEl = document.createElement("span");
+        forecastEl.classList = "card-body";
+        forecastTempEl.textContent = dailyForecast.main.temp + " °F";
+        forecastEl.appendChild(forecastTempEl);
+
+        var forecastHumidityEl = document.createElement("span");
+        forecastHumidityEl.classList = "card-body";
+        forecastHumidityEl.textContent = dailyForecast.main.humidity + "%";
+        forecastEl.appendChild(forecastHumidityEl);
+
+        forecastContainerEl.appendChild(forecastEl)
+    }
 }
 
 $('#search-button').on("click", (event) => {
@@ -53,6 +88,7 @@ $('#search-button').on("click", (event) => {
     currentCity = $('#city-search').val();
     getCurrentWeather(event);
     fiveDayForecast();
+    displayForecast();
 })
 
 function k2f(K) {
