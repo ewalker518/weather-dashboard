4fcbe2e55e525692a09d34cbe9ba440df124f5a8
saltyposaune <ewalkertbone@gmail.com>
2021-11-11 19:49:25 -0700

    recent search buttons display correct data now

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,58 +16,66 @@ var getCurrentWeather = () => {
     let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=" + APIKey;
 
     axios.get(apiURL) // https://axios-http.com/docs/intro
-    .then((response) => {
-        const currentDate = new Date(response.data.dt*1000)
-        const day = currentDate.getDate();
-        const month = currentDate.getMonth();
-        const year = currentDate.getFullYear();
-        cityNameEl.innerHTML = response.data.name + " " + month + '/' + day + '/' + year;
-        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " °F";
-        currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
-        currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-        let lat = response.data.coord.lat;
-        let lon = response.data.coord.lon;
-        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
-        axios.get(UVQueryURL)
-        .then(function(response){
-            let UVIndex = document.createElement("span");
-            UVIndex.setAttribute("class","uv-index")
-            UVIndex.innerHTML = response.data[0].value;
-            currentUVEl.innerHTML = "UV Index: ";
-            currentUVEl.append(UVIndex);
+        .then((response) => {
+            const currentDate = new Date(response.data.dt * 1000)
+            const day = currentDate.getDate();
+            const month = currentDate.getMonth();
+            const year = currentDate.getFullYear();
+            cityNameEl.innerHTML = response.data.name + " " + month + '/' + day + '/' + year;
+            currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " °F";
+            currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            let lat = response.data.coord.lat;
+            let lon = response.data.coord.lon;
+            let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+            axios.get(UVQueryURL)
+                .then(function (response) {
+                    let UVIndex = document.createElement("span");
+                    UVIndex.setAttribute("class", "uv-index")
+                    UVIndex.innerHTML = response.data[0].value;
+                    currentUVEl.innerHTML = "UV Index: ";
+                    currentUVEl.append(UVIndex);
 
-            // if (uvIndex>=0 && uvIndex<3){
-            //     $('#uvVal').attr("class", "uv-favorable");
-            // } else if (uvIndex>=3 && uvIndex<8){
-            //     $('#uvVal').attr("class", "uv-moderate");
-            // } else if (uvIndex>=8){
-            //     $('#uvVal').attr("class", "uv-severe");
-            // }
-        });
-        saveCity();
-        recentSearch(currentCity);
-    })
+                    // if (uvIndex>=0 && uvIndex<3){
+                    //     $('#uvVal').attr("class", "uv-favorable");
+                    // } else if (uvIndex>=3 && uvIndex<8){
+                    //     $('#uvVal').attr("class", "uv-moderate");
+                    // } else if (uvIndex>=8){
+                    //     $('#uvVal').attr("class", "uv-severe");
+                    // }
+                });
+            saveCity();
+            recentSearch(currentCity);
+        })
 }
 
 // 5-day forecast
 var fiveDayForecast = () => {
     var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=" + APIKey;
     fetch(forecastURL)
-    .then(function(response) {
-        response.json()
-        .then(function(data) {
-            displayForecast(data);
+        .then(function (response) {
+            response.json()
+                .then(function (data) {
+                    displayForecast(data);
+                })
         })
-    })
 }
 
-var displayForecast = function(weather) {
+var displayForecast = function (weather) {
     forecastContainerEl.textContent = "";
     var forecast = weather.list;
-    for (let i = 5; i < forecast.length; i=i+8) {
+    for (let i = 5; i < forecast.length; i = i + 8) {
         var dailyForecast = forecast[i];
         var forecastEl = document.createElement("div");
-        forecastEl.classList = "card";
+        forecastEl.classList = "card five-day-forecast";
+
+        var forecastDateEl = document.createElement("span");
+        var forecastDate = new Date(weather.dt * 1000);
+        var forecastMonth = forecastDate.getMonth() + 1;
+        var forecastDay = forecastDate.getDate();
+        var forecastYear = forecastDate.getFullYear();
+        forecastDate.textContent = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+        forecastDateEl.append(forecastDate);
 
         var weatherIcon = document.createElement("img");
         weatherIcon.classList = "card-body";
@@ -89,29 +97,29 @@ var displayForecast = function(weather) {
 }
 
 function saveCity(cities) {
-    localStorage.setItem("Cities", JSON.stringify(cities));    
+    localStorage.setItem("Cities", JSON.stringify(cities));
 }
 
 function k2f(K) {
-    return Math.floor((K - 273.15) *1.8 +32);
+    return Math.floor((K - 273.15) * 1.8 + 32);
 }
 
-var recentSearch = function(recentSearch){
-    console.log("hello");
+var recentSearch = function (recentSearch) {
     recentSearchEl = document.createElement("button");
     recentSearchEl.textContent = recentSearch;
-    recentSearchEl.classList = "btn-light";
+    recentSearchEl.classList = "btn btn-primary recent-search-btn";
     recentSearchEl.setAttribute("recent-city", recentSearch)
     recentSearchEl.setAttribute("type", "submit");
-    console.log(recentSearch);
     recentSearchButtonEl.prepend(recentSearchEl);
 }
 
-var recentSearchHandler = function(event){
+var pastSearchHandler = function(event){
     var city = event.target.getAttribute("recent-city")
+    console.log(city);
     if(city){
-        getCurrentWeather(city);
-        fiveDayForecast(city);
+        currentCity = city;
+        getCurrentWeather(currentCity);
+        fiveDayForecast(currentCity);
     }
 }
 
@@ -121,5 +129,7 @@ $('#search-button').on("click", (event) => {
     getCurrentWeather(event);
     fiveDayForecast();
     searchHistory.push(currentCity);
-    localStorage.setItem("search-history",JSON.stringify(searchHistory));
-})
+    localStorage.setItem("search-history", JSON.stringify(searchHistory));
+});
+
+recentSearchButtonEl.addEventListener("click", pastSearchHandler);
